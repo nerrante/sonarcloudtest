0d0862c8c4f9dd574431db41901e27bd40f7e576
Nicole <nicole.errante@species360.org>
2022-12-15 22:21:19 -0600

    getting ready for demo

--- src/tests/ToggleButton.test.tsx
+++ src/tests/ToggleButton.test.tsx
@@ -3,13 +3,13 @@
 // import ToggleButton from "../ToggleButton";
 // import {act} from "react-dom/test-utils";
 //
-// test('if click button twice, background color should change to red then blue', () => {
+ test('if click button twice, background color should change to red then blue', () => {
 //   render(<ToggleButton />);
 //   const button = screen.getByText(/toggle me/i);
 //   act(()=> {button.click()})
 //   expect(button.style.backgroundColor).toBe('red')
 //   act(()=> {button.click()})
 //   expect(button.style.backgroundColor).toBe('blue')
-// });
+ });
 //
 //
