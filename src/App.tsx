d0667521d38bfa1d6684960fdd999f997fa4b0cb
Nicole <nicole.errante@species360.org>
2022-12-15 21:30:25 -0600

    toggle button

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Hello from "./Hello";
+import ToggleButton from "./ToggleButton";
 
 function App() {
   return (
@@ -35,8 +36,9 @@ function App() {
         >
           Species360!
         </a>
+        <Hello />
+        <ToggleButton disabled={false} />
       </header>
-      <Hello />
     </div>
   );
 }
---
+++ src/ToggleButton.tsx
@@ -0,0 +1,26 @@
+import {useState} from "react";
+
+const ToggleButton = ({}: {
+    disabled: boolean,
+}): JSX.Element => {
+    const [color, setColor] = useState<string>('blue')
+
+    const changeColor = () => {
+        if (color === 'blue') {
+            setColor('red')
+        }
+        else {setColor('blue')}
+    };
+
+    return (
+        <button
+            style={{backgroundColor: color}}
+            aria-label='toggle button'
+            onClick={() => {
+                changeColor();
+            }}
+            data-testid='ToggleButton'>Toggle Me!</button>
+    );
+};
+
+export default ToggleButton
