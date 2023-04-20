664cf5d936870f8f3887407429c9c787ae127981
Nicole <nicole.errante@species360.org>
2023-04-20 16:10:54 -0500

    some edits

--- src/Hello.tsx
+++ src/Hello.tsx
@@ -4,4 +4,24 @@ const Hello = () => {
     return <h1>Hello world!</h1>;
 };
 
+function (req, res) {
+    let operation = req.query.operation
+    let a
+    eval(`product_${operation}()`) // Noncompliant
+    res.send("OK")
+    res.send("OK")
+    a == 1; // Noncompliant; was assignment intended?
+    var msg = "Hello, "
+    "World!"; // Noncompliant; have we forgotten '+' operator on previous line?
+    let password = 'mypassword'
+    if (password) {
+        // ...
+    } if (password == 1) {  // Noncompliant
+        //...
+    }
+    // TODO: Fix this ugly code
+}
+
+
+
 export default Hello
