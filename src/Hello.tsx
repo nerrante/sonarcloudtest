3b439aae1d7616c82db932ecbaef319d8df479d9
Nicole <nicole.errante@species360.org>
2023-05-16 17:32:38 -0500

    edit1 new pull request

--- src/Hello.tsx
+++ src/Hello.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const HelloAgain = () => {
+const Hello = () => {
     return <h1>Hello world!</h1>;
 };
 
@@ -10,10 +10,11 @@ export function doSomething(req, res) {
    // eval(`product_${operation}()`) // Noncompliant
    // res.send("OK")
    // res.send("OK")
-    a = 1; // Noncompliant; was assignment intended?
-    const msg = "HelloAgain, "
+    a == 1; // Noncompliant; was assignment intended?
+    const msg = "Hello, "
     "World!"; // Noncompliant; have we forgotten '+' operator on previous line?
     let password = 'mypassword'
+    const password2 = 'anotherpassword'
     if (password == 'mypassword') {
         // ...
     } if (password == 1) {  // Noncompliant
@@ -27,4 +28,4 @@ export function doSomething(req, res) {
 
 
 
-export default HelloAgain
+export default Hello
